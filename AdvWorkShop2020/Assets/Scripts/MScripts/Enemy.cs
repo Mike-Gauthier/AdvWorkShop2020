e927f680f424901630361fd1b753e98efe7d52fd
MikeGauthierFSU <MGauthierGame@gmail.com>
2020-02-18 14:53:27 -0500

    Mike 2/18 pt 3
    Tower AI & Enemy Class
    Raycast from Towers added

---
+++ AdvWorkShop2020/Assets/Scripts/MScripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+
+
+    private void Update()
+    {
+        //check for player position using magnitude
+        //while player.magnitude 
+        //SpotPlayer
+    }
+
+    void SpotPlayer()
+    {
+        //
+    }
+}
---
+++ AdvWorkShop2020/Assets/Scripts/MScripts/TowerAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAI : MonoBehaviour
+{
+    public float towerSight;
+    //Enemy enemyHealth;
+    //public int shotValue;
+
+    private void Start()
+    {
+        //enemyHealth = GetComponent<Enemy>.health();
+    }
+
+    private void Update()
+    {
+        Ray towerRay = new Ray(transform.position, -transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, -transform.forward * towerSight);
+
+        if (Physics.Raycast(towerRay, out hit, towerSight))
+        {
+            if(hit.collider.tag == "Seeker")
+            {
+                Debug.Log("Enemy Spotted");
+                //AttackEnemy()
+            }
+        }
+
+    }
+    void AttackEnemy()
+    {
+        //shoot enemy
+        //deincrement health
+        //kill if health at zero
+
+        //instantiate shot at enemy position
+        //enemyHealth -= shotValue;
+    }
+}
