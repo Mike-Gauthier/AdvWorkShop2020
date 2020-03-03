ebd39a326c4e7dedcba3b939a815ac755172b3b5
Mike Gauthier <mgauth12@student.fitchburgstate.edu>
2020-03-03 00:31:24 -0500

    Tower AI LockOn

--- AdvWorkShop2020/Assets/Scripts/MScripts/TowerAI.cs
+++ AdvWorkShop2020/Assets/Scripts/MScripts/TowerAI.cs
@@ -6,30 +6,46 @@ public class TowerAI : MonoBehaviour
 {
     public Transform target;
     public float towerSight;
+    public string enemyTag = "enemy";
     //Enemy enemyHealth;
     //public int shotValue;
 
     private void Start()
     {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
         //enemyHealth = GetComponent<Enemy>.health();
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float closest = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < closest)
+            {
+                closest = distance;
+                nearestEnemy = enemy;
+            }
+        }
+        if (nearestEnemy != null && closest <= towerSight)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
     private void Update()
     {
-        Ray towerRay = new Ray(transform.position, -transform.forward);
-        RaycastHit hit;
-
-        Debug.DrawRay(transform.position, -transform.forward * towerSight);
-
-        if (Physics.Raycast(towerRay, out hit, towerSight))
-        {
-            if(hit.collider.tag == "Seeker")
-            {
-                Debug.Log("Enemy Spotted");
-                //AttackEnemy()
-            }
-        }
-
+        if (target == null)
+            return;
     }
     private void OnDrawGizmosSelected()
     {
