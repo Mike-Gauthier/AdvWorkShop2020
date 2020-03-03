d675e79efc3253fdf25954121ecde718967778ed
ZachSimonFSU <60243304+ZachSimonFSU@users.noreply.github.com>
2020-03-03 01:21:06 -0500

    Tower UIs
    I've gotten two tower UI windows started. They can be opened with a button, swapped between new towers and active towers, and closed.

---
+++ AdvWorkShop2020/Assets/Zachary/Scripts/UIWindow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIWindow : MonoBehaviour
+{
+    public Vector3 mousePosition;
+    public Vector3 windowPosition;
+
+    public GameObject cursorTracker;
+    public Transform windowDestination;
+
+    public bool canDrag;
+    public bool dragging;
+    bool attachedToNewPoint;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        windowPosition = gameObject.transform.position;
+
+        if (cursorTracker == null)
+        {
+            cursorTracker = GameObject.FindGameObjectWithTag("Cursor Tracker");
+        }
+
+        if (Input.GetButton("LeftClick") && canDrag)
+        {
+            dragging = true;
+        }
+        else
+        {
+            dragging = false;
+        }
+
+        if (dragging)
+        {
+            attachedToNewPoint = false;
+            Cursor.visible = false;
+
+            gameObject.transform.SetParent(cursorTracker.transform);
+            windowDestination.position = gameObject.transform.position;
+            
+        }
+        else
+        {
+            Cursor.visible = true;
+
+            if (!attachedToNewPoint)
+            {
+                cursorTracker.transform.DetachChildren();
+                gameObject.transform.SetParent(windowDestination);
+                transform.localPosition = Vector3.zero;
+                attachedToNewPoint = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnMouseOver() // Void name from proandrius. URL: https://forum.unity.com/threads/how-to-make-object-detect-mouse-is-over-it.223741/#post-1491314
+    {
+        canDrag = true;
+    }
+}
+
--- AdvWorkShop2020/Assets/Zachary/Scripts/ZachGameManager.cs
+++ AdvWorkShop2020/Assets/Zachary/Scripts/ZachGameManager.cs
@@ -8,15 +8,41 @@ public class ZachGameManager : MonoBehaviour
 
     public GameObject pauseUI;
 
+    public Transform mouseTracker;
+
+    public Vector3 cursorLocation;
+
+    public GameObject activeTowerUI;
+    public GameObject newTowerUI;
+
+    public bool isActiveTowerUICurrent;
+
+    public GameObject objectToClose;
+
     // Start is called before the first frame update
     void Start()
     {
         paused = false;
+        mouseTracker.position = Input.mousePosition;
+
+        if (isActiveTowerUICurrent && newTowerUI.activeInHierarchy)
+        {
+            newTowerUI.SetActive(false);
+        }
+
+        if (!isActiveTowerUICurrent && activeTowerUI.activeInHierarchy)
+        {
+            activeTowerUI.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        cursorLocation = Input.mousePosition;
+        mouseTracker.position = cursorLocation;
+        // cursorLocation = Camera.main.ScreenToWorldPoint(cursorLocation);
+
         if (paused)
         {
             Time.timeScale = 0.0f; // This stops the flow of time in game completely allowing the game to pause. Also allows other scripts to detect if the game is paused. Sounds can still play so more code may have to be written to deal with sound when the game is paused. On a plus side, in my last project pausing the game did not stop my weapon changing code from working so switching tools is very possible even with the time scale at zero. - Zachary Simon
@@ -43,6 +69,76 @@ public class ZachGameManager : MonoBehaviour
         else
         {
             paused = false;
+        }
+    }
+
+    public void CloseUIWindow(GameObject objectToClose)
+    {
+        objectToClose.SetActive(false);
+
+        ReactivateCursor();
+    }
+
+    public void OpenTowerUI()
+    {
+        if (newTowerUI.activeInHierarchy)
+        {
+            CloseUIWindow(newTowerUI);
+        }
+        else if (activeTowerUI.activeInHierarchy)
+        {
+            CloseUIWindow(activeTowerUI);
+        }
+        else
+        {
+            if (isActiveTowerUICurrent)
+            {
+                OpenActiveTowerUI();
+            }
+            else
+            {
+                OpenNewTowerUI();
+            }
+        }
+    }
+
+    public void OpenActiveTowerUI()
+    {
+        activeTowerUI.transform.position = newTowerUI.transform.position;
+
+        if(newTowerUI.activeInHierarchy)
+        {
+            newTowerUI.SetActive(false);
+        }
+
+        activeTowerUI.SetActive(true);
+
+        isActiveTowerUICurrent = true;
+
+        ReactivateCursor();
+    }
+
+    public void OpenNewTowerUI()
+    {
+        newTowerUI.transform.position = activeTowerUI.transform.position;
+
+        if (activeTowerUI.activeInHierarchy)
+        {
+            activeTowerUI.SetActive(false);
+        }
+
+        newTowerUI.SetActive(true);
+
+        isActiveTowerUICurrent = false;
+
+        ReactivateCursor();
+    }
+
+    void ReactivateCursor()
+    {
+        if (Cursor.visible == false)
+        {
+            Cursor.visible = true;
         }
     }
 }
